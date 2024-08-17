dbe802e3c26295fb8e96d12c8def5aa44f96e3db
Cameron Wright <ccameronwright@gmail.com>
2024-08-17 17:48:59 -0600

    Script now can download the last 7 days and record it into the database.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import requests
 import sqlite3
+import logging
 from datetime import datetime
 from bs4 import BeautifulSoup
 
@@ -9,27 +10,37 @@ from bs4 import BeautifulSoup
 third_rock_playlist_url = "https://onlineradiobox.com/us/thirdrock/playlist/"
 dir_path = os.getcwd()
 database_path = os.path.join(dir_path, "data/thirdRockRadio.db")
+logger = logging.getLogger(__name__)
 
 def run():
-    # Loop through all days, with 0 being today, and 6 being 7 days ago.
-    for i in range(0, 6):
-        page = requests.get(f"{third_rock_playlist_url}/{i}")
+    # Get songs for today
+    page = requests.get(third_rock_playlist_url)
+    parse_page(page.content)
+    # Loop through all days, with 1 being yesturday, and 6 being 7 days ago.
+    for i in range(1, 6):
+        logger.debug(f"Scraping {i} day's ago. URL: {third_rock_playlist_url}/{i}")
+        page = requests.get(f"{third_rock_playlist_url}{i}")
         parse_page(page.content)
 
 
 def test_run():
-    index_path = os.path.join(dir_path, "data/yesturday.html")
-    # Check if file doesn't exist, if it doesn't bounce.
-    if not os.path.exists(index_path):
-        print(f"index.html doesn't exist in data dir, run the downloadPage.sh shell script in tools dir and retry.")
-        exit(0)
-    with open(index_path, "r") as file:
-        index_content = file.read()
-    parse_page(index_content)
+    # Test today and yesturday
+    for i in ("data/index.html", "data/yesturday.html"):
+        print(f"Scanning {i}")
+        index_path = os.path.join(dir_path, "data/index.html")
+        # Check if file doesn't exist, if it doesn't bounce.
+        if not os.path.exists(index_path):
+            print(f"index.html doesn't exist in data dir, run the downloadPage.sh shell script in tools dir and retry.")
+            continue
+        with open(index_path, "r") as file:
+            index_content = file.read()
+        parse_page(index_content)
+    
 
 def parse_page(content: str):
     soup = BeautifulSoup(content, "html.parser")
     date = soup.find_all("ul", {"class": "playlist__schedule"})
+    print(date)
     date = date[0].find("span").text.split(" ", 1)
     day_month = date[1].split(".", 1)
     day = int(day_month[0])
@@ -52,7 +63,7 @@ def parse_page(content: str):
         band_name = band_and_name[0].strip()
         song_name = band_and_name[1].strip()
         time = time.text.strip()
-        print(f"\"{song_name}\" by \"{band_name}\" played at {time}")
+        # print(f"\"{song_name}\" by \"{band_name}\" played at {time}")
         epoch = get_epoch(month, day, time)
         add_entry(song_name, band_name, epoch)
 
@@ -86,8 +97,9 @@ def setup_sqlite():
     # Create song table
     cur.execute("""CREATE TABLE IF NOT EXISTS song(
                 song_id INTEGER PRIMARY KEY,
-                song_name TEXT NOT NULL UNIQUE,
+                song_name TEXT NOT NULL,
                 band_id INTEGER NOT NULL,
+                UNIQUE(song_name, band_id),     
                 FOREIGN KEY(band_id) REFERENCES band(band_id)
                 )""")
     # Create broadcast
@@ -114,6 +126,7 @@ def add_band(name: str) -> int:
     band_id = res.fetchall()
     # If band id is null, add band
     if len(band_id) == 0:
+        logger.debug(f"Didn't find band \"{name}\" in database, inserting it.")
         res = cur.execute(f"INSERT INTO band (band_name) VALUES ('{name}') ON CONFLICT DO NOTHING RETURNING band_id")
         band_id = res.fetchall()
     band_id = band_id[0][0]
@@ -129,6 +142,7 @@ def add_song(name: str, band_id: int) -> int:
     song_id = res.fetchall()
     # If song id is null, add song
     if len(song_id) == 0:
+        logger.debug(f"Didn't find song \"{name}\" for band id ({band_id}) in database, inserting it.")
         res = cur.execute(f"INSERT INTO song (song_name, band_id) VALUES ('{name}', {band_id}) ON CONFLICT DO NOTHING RETURNING song_id")
         song_id = res.fetchall()
     song_id = song_id[0][0]
@@ -142,6 +156,7 @@ def add_broadcast(broadcast_time: int, song_id: int):
     broadcast_id = res.fetchall()
     if len(broadcast_id) > 0:
         broadcast_id = broadcast_id[0][0]
+        logger.debug(f"Didn't find broadcast epoch {broadcast_time} for song_id ({song_id}) in database, inserting it.")
         cur.execute(f"INSERT INTO song_broadcast (song_id, broadcast_id) VALUES ({song_id}, {broadcast_id}) ON CONFLICT DO NOTHING")
     con.commit()
 
@@ -165,7 +180,12 @@ def check_web_robot():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--test', help='Uses pre-downloaded song list, used for testing purposes.', action="store_true")
+    parser.add_argument('-d', '--debug', help='Set log level to debug, default is INFO', action="store_false")
     args = parser.parse_args()
+    if args.debug == True:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
     print(f"Collecting Third Rock Radio Song's at { datetime.now() }")
     if not args.test:
         run()
@@ -173,5 +193,5 @@ if __name__ == "__main__":
         print("Running Test")
         test_run()
     print("********************************************")
-    for song in get_song_counts():
-        print(f"Song \"{song[0]}\" by \"{song[1]}\" played {song[2]}")
+    # for song in get_song_counts():
+    #     print(f"Song \"{song[0]}\" by \"{song[1]}\" played {song[2]}")
--- tools/downloadPage.sh
+++ tools/downloadPage.sh
@@ -4,4 +4,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEST_DIR=$SCRIPT_DIR/../data
 [ -d "$DEST_DIR" && ! -f "$DEST_DIR/index.html" ] || mkdir $DEST_DIR
-wget https://onlineradiobox.com/us/thirdrock/playlist/ -P $DEST_DIR
+# Download today
+[ ! -f "$DEST_DIR/index.html" ] && wget https://onlineradiobox.com/us/thirdrock/playlist/ -P $DEST_DIR
+# Download yesturday
+[ ! -f "$DEST_DIR/yesturday.html" ] && wget https://onlineradiobox.com/us/thirdrock/playlist/1 -O $DEST_DIR/yesturday.html
